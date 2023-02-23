3b28a579446cdccc76669de2f3ebbc3e0fcc04ed
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-02-23 13:58:58 -0800

    Create 1491. Average Salary Excluding the Minimum and Maximum Salary.js

---
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.js
@@ -0,0 +1,12 @@
+var average = function(salary) {
+  let a=  salary.sort();
+    //1000, 2000, 3000,4000
+
+a.shift();
+a.pop();
+let r=a.reduce((num,num1)=>{
+    num=num+num1
+    return num
+})
+return r/a.length
+};
